9c256f7b03217aa0cd233e73b0beb41304bff6e5
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-12-10 20:40:17 -0800

    add routes, app files

---
+++ js/app.js
@@ -0,0 +1,5 @@
+(function() {
+  'use strict';
+
+  angular.module('mp3Playground', ['ui.router', 'ngResource']);
+}());
---
+++ js/config/routes.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular.module('mp3Playground')
+    .config(MainRouter);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function MainRouter($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'js/templates/home.html'
+      })
+      
+  }
+}());
