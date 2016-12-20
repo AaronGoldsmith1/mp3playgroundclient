6d6ffb5b87b41fd24499c5f6a3ada09436370413
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-12-19 20:46:38 -0800

    skip songs button

--- js/components/playlistPlayer/playlistPlayer.js
+++ js/components/playlistPlayer/playlistPlayer.js
@@ -12,6 +12,7 @@ playlistPlayer.$inject = ['$http', '$element']
 
 function playlistPlayer($http, $element){
   var ctrl = this;
+  ctrl.currentSong = null;
   var $container = $element.find('div')[0];
   var wavesurfer = WaveSurfer.create({
     container: $container,
@@ -20,10 +21,28 @@ function playlistPlayer($http, $element){
 
 
   ctrl.playSong = function(song){
+    if (ctrl.currentSong == song) {
+      wavesurfer.seekTo(0)
+      wavesurfer.play()
+    } else {
     wavesurfer.load(song.url);
     wavesurfer.on('ready', function(){
+      ctrl.currentSong = song
       wavesurfer.play();
-    });
+      });
+    }
+  }
+
+  ctrl.playPause = function(){
+    wavesurfer.playPause();
+  }
+
+  ctrl.skipBackward = function(){
+    wavesurfer.skipBackward();
+  }
+
+  ctrl.skipForward = function() {
+    wavesurfer.skipForward();
   }
 
   ctrl.toggleMute = function(){
@@ -32,6 +51,30 @@ function playlistPlayer($http, $element){
 
   ctrl.pause = function(){
     wavesurfer.pause();
+  }
+
+  ctrl.nextSong = function(){
+
+    targetSong = _.indexOf(ctrl.songs, ctrl.currentSong)
+
+    if (targetSong < 0){
+      return
+    }
+
+    targetSong++
+    // Get the index of current in ctrl.songs
+
+    //Get the song at the next index
+    if (targetSong < ctrl.songs.length) {
+      ctrl.playSong(ctrl.songs[targetSong])
+    }
+    // if there is a song at the next index
+
+    //call ctrl.playSong on it
+  }
+
+  ctrl.prevSong = function(){
+
   }
 
 }
